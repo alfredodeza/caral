0834e33ee51f648c3ebf81d541e4ae669824c473
Alfredo Deza <alfredodeza@gmail.com>
2011-11-17 21:39:17 -0500

    removing unneeded file

--- caral/log.py
+++
@@ -1,8 +0,0 @@
-from logging import getLogger
-
-## Set logger objects for all modules
-middleware  = getLogger('pypi.middleware')
-util        = getLogger('pypi.util')
-database    = getLogger('pypi.database')
-server      = getLogger('pypi.web.server')
-model       = getLogger('pypi.web.model')
