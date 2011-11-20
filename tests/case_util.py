b91a3abf36bc7c0b87201325e3a868ae6eb32437
Alfredo Deza <alfredodeza@gmail.com>
2011-11-19 20:03:36 -0500

    add more tests for util

--- tests/case_util.py
+++ tests/case_util.py
@@ -42,3 +42,46 @@ describe "browsing the PyPi index skips some files":
         pkg = "httpslib"
 
         assert self.browse.should_skip_pkg(pkg) is False
+
+
+describe "browsing PyPi index skips some urls":
+
+    before each:
+        self.browse = util.BrowsePyPi('', '')
+
+    it "does not skip tar.gz urls":
+        href = "http://example.com/file.tar.gz"
+
+        assert self.browse.should_skip_href(href) is False
+
+    it "does not skip .tar.bz2 urls":
+        href = "http://example.com/file.tar.bz2"
+
+        assert self.browse.should_skip_href(href) is False
+
+    it "does not skip .tar urls":
+        href = "http://example.com/file.tar"
+
+        assert self.browse.should_skip_href(href) is False
+
+    it "does not skip .zip urls":
+        href = "http://example.com/file.zip"
+
+        assert self.browse.should_skip_href(href) is False
+
+    it "does not skip .tgz urls":
+        href = "http://example.com/file.tgz"
+
+        assert self.browse.should_skip_href(href) is False
+
+    it "does skip non compressed file urls":
+        href = "http://example.com/file.bar"
+
+        assert self.browse.should_skip_href(href) is True
+
+    it "does not skip when is valid but has trailing slashes":
+        href = "http://example.com/file.zip/"
+
+        assert self.browse.should_skip_href(href) is False
+
+
