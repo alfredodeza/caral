544192a2fe78fe6ef589e54d0e71937c3fefa3e5
Alfredo Deza <alfredodeza@gmail.com>
2011-11-19 19:45:42 -0500

    add more tests for skipping files

--- tests/case_util.py
+++ tests/case_util.py
@@ -1,14 +1,44 @@
-from pecan import conf
-from pecan.testing import set_test_app
 from caral import util
 
 
 
-describe "browsing the PyPi index":
+describe "browsing the PyPi index skips some files":
 
     before each:
-        self.app = set_test_app('testing.py')
+        self.browse = util.BrowsePyPi('', '')
+
 
     it "should skip htm files":
-        assert conf.server.port == '80'
-        assert self.app.get('/') == '200'
+        pkg = "foobar.htm"
+
+        assert self.browse.should_skip_pkg(pkg) is True
+
+    it "should skip html files":
+        pkg = "foobar.html"
+
+        assert self.browse.should_skip_pkg(pkg) is True
+
+    it "should skip org files":
+        pkg = "foobar.org"
+
+        assert self.browse.should_skip_pkg(pkg) is True
+
+    it "should skip net files":
+        pkg = "foobar.net"
+
+        assert self.browse.should_skip_pkg(pkg) is True
+
+    it "should skip http files":
+        pkg = "http://foobar.me"
+
+        assert self.browse.should_skip_pkg(pkg) is True
+
+    it "should not skip files with http that are non urls":
+        pkg = "httplib"
+
+        assert self.browse.should_skip_pkg(pkg) is False
+
+    it "should not skip files with https that are non urls":
+        pkg = "httpslib"
+
+        assert self.browse.should_skip_pkg(pkg) is False
